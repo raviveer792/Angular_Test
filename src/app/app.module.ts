c74d60ed5f5dd35c5d86244db6b71b1685965e2e
Ravi.Suryavanshi <Ravi.Suryavanshi@rws.com>
2022-09-16 19:00:28 +0530

    job and job-form components are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JobComponent } from './components/job/job.component';
+import { JobFormComponent } from './components/job-form/job-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobComponent,
+    JobFormComponent
   ],
   imports: [
     BrowserModule,
