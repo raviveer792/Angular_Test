c66d6e81ad258214bbe56b7bf3bffcd2edc7052e
Ravi.Suryavanshi <Ravi.Suryavanshi@rws.com>
2022-09-16 19:04:24 +0530

    Created routes for job components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JobFormComponent } from './components/job-form/job-form.component';
+import { JobComponent } from './components/job/job.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/jobs',
+    pathMatch: 'full',
+  },
+  {
+    path: 'jobs',
+    component: JobComponent,
+    children: [
+      {
+        path: 'new',
+        component: JobFormComponent,
+      },
+    ],
+  },
+  {
+    path: 'jobs/:id',
+    component: JobComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
